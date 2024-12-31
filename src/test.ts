eae8039aac0f03dc5f6ebc1d46cd6c3c85f4908d
Victor Teo <victor.teo@fillmula.com>
2024-12-31 21:31:43 +0800

    add missing tests

--- src/test.ts
+++ src/test.ts
@@ -142,3 +142,48 @@ test("inherited for inherited component with multiple inheritances", (t) => {
   const result = renderToString(element)
   t.is(result, '<div class="foo0 foo1 foo2 foo bar"></div>')
 })
+
+test("inherited for HTML component with props", (t) => {
+  const component = (inherited as any).div(
+    (props: any) => ({ key0: props.key0 }))
+  const element = createElement(component, { key0: 'value0' })
+  const result = renderToString(element)
+  t.is(result, '<div key0="value0"></div>')
+})
+
+test("inherited for React component with props", (t) => {
+  const base = (props: HTMLProps<HTMLDivElement>) => createElement('div', props)
+  const component = (inherited as any)(base)(
+    (props: any) => ({ key0: props.key0 }))
+  const element = createElement(component, { key0: 'value0' })
+  const result = renderToString(element)
+  t.is(result, '<div key0="value0"></div>')
+})
+
+test("inherited for inherited component with props", (t) => {
+  const base = (inherited as any).div(
+    (props: any) => ({ key0: props.key0 }))
+  const component = (inherited as any)(base)(
+    (props: any) => ({ key1: props.key1 }))
+  const element = createElement(component, { key0: 'value0', key1: 'value1' })
+  const result = renderToString(element)
+  t.is(result, '<div key0="value0" key1="value1"></div>')
+})
+
+test(
+  "inherited for inherited component with props, with multiple inheritances",
+  (t) => {
+  const base0 = (inherited as any).div(
+    (props: any) => ({ className: props.foo0 }))
+  const base1 = (inherited as any)(base0)(
+    (props: any) => ({ className: props.foo1 }))
+  const base = (inherited as any)(base1)(
+    (props: any) => ({ className: props.foo2 }))
+  const component = (inherited as any)(base)(
+    (props: any) => ({ className: props.arg }))
+  const element = createElement(component, {
+    foo0: "foo0", foo1: "foo1", foo2: "foo2", arg: "bar"
+  })
+  const result = renderToString(element)
+  t.is(result, '<div class="foo0 foo1 foo2 bar" foo0="foo0" foo1="foo1" foo2="foo2" arg="bar"></div>')
+})
