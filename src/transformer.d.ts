e00452faa9274cac6b6f10f0b34ca58abdcc2f71
Victor Teo <victor.teo@fillmula.com>
2025-01-04 18:25:47 +0800

    update transformer types

--- src/transformer.d.ts
+++ src/transformer.d.ts
@@ -1,11 +1,14 @@
-import { ReactNode } from 'react'
+import type { JSX, ReactNode } from 'react'
+import type { IntrinsicElements } from './intrinsic'
 
 export type ComponentTransformer<B> = {
   <P extends NoInfer<B>, N extends NoInfer<ReactNode>>(component: (props: P) => N): (props: P) => N
-//  <C extends string, CP extends ComponentProps<C>, P extends B & CP>(component: C): (props: P) => N
+} & {
+  readonly [N in keyof JSX.IntrinsicElements]: (props: JSX.IntrinsicElements[N]) => IntrinsicElements[N]
 }
 
 export type ComponentTransformerWithAdditionalProps<T, B> = {
   <P extends NoInfer<B>, N extends NoInfer<ReactNode>>(component: (props: P) => N): (props: T & P) => N
-//  <C extends string, CP extends ComponentProps<C>, P extends B & CP>(component: C): (props: T & P) => N
+} & {
+  readonly [N in keyof JSX.IntrinsicElements]: (props: JSX.IntrinsicElements[N] & T) => IntrinsicElements[N]
 }
